8b5d47808a9496d285492117e27b1f5ed0f23cb7
sabbirquince <59896171+sabbirquince@users.noreply.github.com>
2021-06-09 02:02:51 +0600

    refactored code

--- JS/index.js
+++ JS/index.js
@@ -1,42 +1,6 @@
-// FAKE TABLE DATA
+// FAKE TABLE DATA IS ON 'fakeTableData.js' FILE AS AN ARRAY
 
-// let fakeTableData = [
-//   {
-//     id: "1",
-//     name: "John",
-//     email: "john34@outlook.com",
-//     phone: "+884870025",
-//     totalScan: 4,
-//     totalSchedule: 3,
-//     status: "active",
-//     role: "Moderator",
-//     lastLogin: "3 minutes ago",
-//   },
-//   {
-//     id: "2",
-//     name: "Emma",
-//     email: "emma34@outlook.com",
-//     phone: "+88457526",
-//     totalScan: 6,
-//     totalSchedule: 5,
-//     status: "active",
-//     role: "Editor",
-//     lastLogin: "2 minutes ago",
-//   },
-//   {
-//     id: "3",
-//     name: "Rachel",
-//     email: "rachel34@outlook.com",
-//     phone: "+88423526",
-//     totalScan: 2,
-//     totalSchedule: 4,
-//     status: "active",
-//     role: "Moderator",
-//     lastLogin: "3 minutes ago",
-//   },
-// ];
-
-// SHOWING DATA TO THE DOM
+/////////////////// SHOWING DATA TO THE DOM ///////////////////
 const tableBody = document.querySelector(".my-table-body");
 
 const showInTable = () => {
@@ -58,37 +22,65 @@ const showInTable = () => {
         </td>
       </tr>
         `;
-
-    console.log(fakeTableData);
   });
 };
 
 showInTable();
 
-// POPUP BUTTON SHOW FUNCTIONALITY
-const addUserButton = document.querySelector(".my-addUser-btn");
-const addUserPopup = document.querySelector(".my-popup");
-const addUserPopupClose = document.querySelector(".my-popupClose-btn");
-const tableContainer = document.getElementById("my-table-container");
+//////////////////// EDITING A USER FROM THE TABLE LIST ////////////////////
+const editUserPopup = document.querySelector(".edit-user-popup");
+const editUserClosePopup = document.querySelector(".editUser-close-btn");
 
-addUserButton.addEventListener("click", () => {
-  addUserPopup.classList.add("popup-show");
-  addUserPopup.classList.remove("popup-hidden");
-  tableContainer.classList.add("my-table-container");
-});
+const editName = document.getElementById("editName");
+const editEmail = document.getElementById("editEmail");
+const editPhone = document.getElementById("editPhone");
+const editRole = document.getElementById("editRole");
+const editId = document.getElementById("editId");
 
-// POPUP BUTTON CLOSE FUNCTIONALITY
-const closeAddUserPopup = () => {
-  addUserPopup.classList.add("popup-hidden");
-  addUserPopup.classList.remove("popup-show");
-  tableContainer.classList.remove("my-table-container");
+// this function is implemented on row's edit button (line: 21)
+const editUser = (id) => {
+  const editableUser = fakeTableData.filter((user) => user.id === id);
+
+  editName.value = editableUser[0].name;
+  editEmail.value = editableUser[0].email;
+  editPhone.value = editableUser[0].phone;
+  editRole.value = editableUser[0].role;
+  editId.value = editableUser[0].id;
+
+  popupShow(editUserPopup);
 };
 
-addUserPopupClose.addEventListener("click", () => {
-  closeAddUserPopup();
+const editedFormSubmit = (e) => {
+  e.preventDefault();
+  const id = e.target.id.value;
+
+  const editedUser = fakeTableData.filter((user) => user.id === id)[0];
+
+  const pushUser = {
+    ...editedUser,
+    name: e.target.name.value,
+    email: e.target.email.value,
+    phone: e.target.phone.value,
+    role: e.target.role.value,
+  };
+
+  fakeTableData.splice(id - 1, 1, pushUser);
+  popupClose(editUserPopup);
+  showInTable();
+};
+
+const editForm = document.getElementById("editForm");
+
+editForm.addEventListener("submit", (e) => {
+  editedFormSubmit(e);
 });
 
-// ADDING USER TO THE TABLE LIST
+editUserClosePopup.addEventListener("click", (event) => {
+  event.preventDefault();
+  popupClose(editUserPopup);
+});
+
+//////////////////// ADDING USER TO THE TABLE LIST //////////////////////////
 const addUserBtn = document.querySelector(".add-user-btn");
 
 const addName = document.getElementById("addName");
@@ -109,67 +101,34 @@ addUserBtn.addEventListener("click", () => {
     lastLogin: "3 minutes ago",
   });
 
-  closeAddUserPopup();
+  popupClose(addUserPopup);
   showInTable();
 });
 
-// EDITING A USER FROM THE TABLE LIST
-const editUserPopup = document.querySelector(".edit-user-popup");
-const editUserClosePopup = document.querySelector(".editUser-close-btn");
+// VARIABLE USED IN POPUP SHOW & CLOSE
+const addUserButton = document.querySelector(".my-addUser-btn");
+const addUserPopup = document.querySelector(".my-popup");
+const addUserPopupClose = document.querySelector(".my-popupClose-btn");
+const tableContainer = document.getElementById("my-table-container");
 
-const editName = document.getElementById("editName");
-const editEmail = document.getElementById("editEmail");
-const editPhone = document.getElementById("editPhone");
-const editRole = document.getElementById("editRole");
-const editId = document.getElementById("editId");
-
-const editUser = (id) => {
-  const editableUser = fakeTableData.filter((user) => user.id === id);
-  console.log(editableUser);
-
-  editName.value = editableUser[0].name;
-  editEmail.value = editableUser[0].email;
-  editPhone.value = editableUser[0].phone;
-  editRole.value = editableUser[0].role;
-  editId.value = editableUser[0].id;
-
-  editUserPopup.classList.add("popup-show");
-  editUserPopup.classList.remove("popup-hidden");
+///////////////////// POPUP BUTTON SHOW FUNCTIONALITY /////////////////////////
+const popupShow = (popupClassName) => {
+  popupClassName.classList.add("popup-show");
+  popupClassName.classList.remove("popup-hidden");
   tableContainer.classList.add("my-table-container");
 };
 
-const editedSubmit = (e) => {
-  e.preventDefault();
-  const id = e.target.id.value;
-
-  const editedUser = fakeTableData.filter((user) => user.id === id)[0];
-  const pushUser = {
-    ...editedUser,
-    name: e.target.name.value,
-    email: e.target.email.value,
-    phone: e.target.phone.value,
-    role: e.target.role.value,
-  };
-
-  fakeTableData.splice(id - 1, 1, pushUser);
-  editPopupClose();
-  showInTable();
-};
-
-const editForm = document.getElementById("editForm");
-
-editForm.addEventListener("submit", (e) => {
-  editedSubmit(e);
+addUserButton.addEventListener("click", () => {
+  popupShow(addUserPopup);
 });
 
-// CLOSING EDIT USER POPUP
-const editPopupClose = () => {
-  editUserPopup.classList.remove("popup-show");
-  editUserPopup.classList.add("popup-hidden");
+/////////////////////// POPUP BUTTON CLOSE FUNCTIONALITY ///////////////////////
+const popupClose = (popupClassName) => {
+  popupClassName.classList.add("popup-hidden");
+  popupClassName.classList.remove("popup-show");
   tableContainer.classList.remove("my-table-container");
 };
 
-editUserClosePopup.addEventListener("click", (event) => {
-  event.preventDefault();
-  editPopupClose();
+addUserPopupClose.addEventListener("click", () => {
+  popupClose(addUserPopup);
 });
